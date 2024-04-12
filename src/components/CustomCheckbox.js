22bab48bee666f257a1900251df0b7eb094018fb
Stephen Ingham <smi505@york.ac.uk>
2024-04-12 22:55:57 +0100

    chore: changes to get xo pre commit step to pass

---
+++ src/components/CustomCheckbox.js
@@ -0,0 +1,13 @@
+/* import {faCircleCheck as unFilledCheckBox} from '@fortawesome/free-regular-svg-icons';
+import {faCircleCheck as filledCheckBox} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'; */
+import React from 'react';
+
+const CustomCheckBox = () => {
+	return (
+    <div>Custom Check Box</div>
+	);
+};
+
+export default CustomCheckBox;
+
