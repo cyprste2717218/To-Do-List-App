733696fa4c126b863e3dab87dc53dbae41b233ae
Stephen Ingham <smi505@york.ac.uk>
2024-04-12 22:37:55 +0100

    chore: lined up elements in to do item and set to do item to span 85vw width

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -17,30 +17,35 @@ const ToDoListItem = ({
     const [toDoChecked, setToDoChecked] = useState(checkbox);
 
     return (
-        <div>
-            <span key={itemKey}>
+        <div style={{width: '88vw', border: '1px solid gray', borderRadius: '16px', display: 'flex', padding: '10px'}}>
+            <span key={itemKey} style={{marginTop: 'auto', marginBottom: 'auto', flexDirection: 'row', display: 'flex'}}>
 
-                <label htmlFor={`check${itemKey}`} />
-                <input
-                    style={{height: '25px', width: '25px'}}
-                    id={`check${itemKey}`}
-                    type='checkbox' checked={toDoChecked} onChange={e => handleToDoContentChange(
-                e.target.checked, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
-            />
+                <div style={{marginTop: 'auto', marginBottom: 'auto', width: '15%'}}>
+                    <label htmlFor={`check${itemKey}`} />
+                    <input
+                        style={{height: '25px', width: '25px'}}
+                        id={`check${itemKey}`}
+                        type='checkbox' checked={toDoChecked} onChange={e => handleToDoContentChange(
+                    e.target.checked, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
+                    />
+                </div>
+                <div style={{width: '75%'}}>
+                    <label htmlFor={`text${itemKey}`} />
+                    <input
+                        style={{padding: '10px'}}
+                        id={`text${itemKey}`}
+                        type='text' value={itemText} onChange={e => handleToDoContentChange(
+                    e.target.value, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
+                    />
+                </div>
 
-                <label htmlFor={`text${itemKey}`} />
-                <input
-                    style={{padding: '10px'}}
-                    id={`text${itemKey}`}
-                    type='text' value={itemText} onChange={e => handleToDoContentChange(
-                e.target.value, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
-                 />
-
-                <DeleteButton
-                    allToDos={allToDos}
-                    setAllToDos={setAllToDos}
-                    itemKey={itemKey}
-            />
+                <div style={{marginTop: 'auto', marginBottom: 'auto', width: '10%'}}>
+                    <DeleteButton
+                        allToDos={allToDos}
+                        setAllToDos={setAllToDos}
+                        itemKey={itemKey}
+                    />
+                </div>
             </span>
         </div>
     );
