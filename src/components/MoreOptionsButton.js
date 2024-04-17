6161c952afbcbc2bc7bfec8d8018dd9f0668d4b1
Stephen Ingham <smi505@york.ac.uk>
2024-04-17 22:52:47 +0100

    chore: created base .js file for MoreOptionsButton component

---
+++ src/components/MoreOptionsButton.js
@@ -0,0 +1,15 @@
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {useState} from 'react';
+
+const MoreOptionsButton = () => {
+	const [showMoreOptions, setShowMoreOptions] = useState(false);
+
+	return (
+    <div>
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+    </div>
+	);
+};
+
+export default MoreOptionsButton;
