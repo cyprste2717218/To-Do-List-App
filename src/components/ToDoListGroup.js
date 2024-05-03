910e48b1f273a361cf40731aebfc3c43a045616f
Stephen Ingham <stephenmichael833@gmail.com>
2024-05-04 00:41:26 +0100

    Fix/fix UI issues (#10)
    * chore:added wide form factor screenshots of to-do list app and updated theme ccolour in metadata for pwa display
    
    * chore: changed manifest to point to favicon.ico in icons section
    
    * chore: set to-do list items to add from the top of the screen and fixed to-do list items vertical overflow bug preventing visibility of all to-do list items when too many added
    
    * chore: set fixed hieght for ooter tpor prevent overlap of add to-do button with rendered to-dos

--- src/components/ToDoListGroup.js
+++ src/components/ToDoListGroup.js
@@ -3,44 +3,46 @@ import React from 'react';
 import ToDoListItem from './ToDoListItem';
 
 const ToDoListGroup = ({
-    allToDos,
-    setAllToDos,
-    onUpdatedToDoItem,
-    sortStatus
-  }) => {
-    const allKeys = Object.keys(localStorage);
-    const orderedKeys = allKeys.map(num => Number.parseInt(num, 10)).sort((a, b) => a - b);
-    let newKeys = [];
+  allToDos,
+  setAllToDos,
+  onUpdatedToDoItem,
+  sortStatus
+}) => {
+  const allKeys = Object.keys(localStorage);
+  const orderedKeys = allKeys.map(num => Number.parseInt(num, 10)).sort((a, b) => a - b);
+  let newKeys = [];
 
-    if (sortStatus) { // could do with allowing completed todos to go at the end of the completed pile, rather than being specifically sorted based on index
-      const orderedKeysCopy = orderedKeys;
-      const checkedToDoKeys = orderedKeysCopy.filter(toDoKey => JSON.parse(localStorage.getItem(toDoKey))[0]);
-      const uncheckedToDoKeys = orderedKeysCopy.filter(toDoKey => !checkedToDoKeys.includes(toDoKey));
-      const sortedKeys = checkedToDoKeys.concat(uncheckedToDoKeys);
-      newKeys = sortedKeys;
-    } else {
-      newKeys = orderedKeys;
-    }
+  if (sortStatus) { // could do with allowing completed todos to go at the end of the completed pile, rather than being specifically sorted based on index
+    const orderedKeysCopy = orderedKeys;
+    const checkedToDoKeys = orderedKeysCopy.filter(toDoKey => JSON.parse(localStorage.getItem(toDoKey))[0]);
+    const uncheckedToDoKeys = orderedKeysCopy.filter(toDoKey => !checkedToDoKeys.includes(toDoKey));
+    const sortedKeys = checkedToDoKeys.concat(uncheckedToDoKeys);
+    newKeys = sortedKeys;
+  } else {
+    newKeys = orderedKeys;
+  }
 
-    return (
-        <div style={{display: 'flex', justifyContent: 'center', height: 'calc(100vh - 300px)', overflowY: 'scroll'}}>
-            <div id='to-do-group-div' style={{width: '80vw'}}>
-                {newKeys.map(index => {
-              return (
-                  <ToDoListItem
-                      key={index}
-                      itemKey={index}
-                      setChangedToDoItem={onUpdatedToDoItem}
-                      text={JSON.parse(localStorage.getItem(index))[1]}
-                      checkbox={JSON.parse(localStorage.getItem(index))[0]}
-                      allToDos={allToDos}
-                      setAllToDos={setAllToDos}
-              />
-);
-          })}
-            </div>
-        </div>
-    );
-  };
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', height: 'calc(100vh - 280px)', overflowY: 'scroll' }}>
+      <div id='to-do-group-div' style={{ width: '80vw' }}>
+        <br></br>
+        {newKeys.map(index => {
+          return (
+            <ToDoListItem
+              key={index}
+              itemKey={index}
+              setChangedToDoItem={onUpdatedToDoItem}
+              text={JSON.parse(localStorage.getItem(index))[1]}
+              checkbox={JSON.parse(localStorage.getItem(index))[0]}
+              allToDos={allToDos}
+              setAllToDos={setAllToDos}
+            />
+          );
+        })}
+        <br></br>
+      </div>
+    </div>
+  );
+};
 
-  export default ToDoListGroup;
+export default ToDoListGroup;
