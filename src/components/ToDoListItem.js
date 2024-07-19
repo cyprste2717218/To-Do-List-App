8d3e098ab7e3ebafd3e9a1271ca48e7f0a2efee7
Stephen Ingham <stephenmichael833@gmail.com>
2024-07-19 19:07:33 +0100

    Changed textbox to textarea and setup relevant rendering logic for increasing height based on text content

--- src/components/ToDoListItem.js
+++ src/components/ToDoListItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import handleToDoContentChange from '../utilities/changingToDoMethods';
 
@@ -13,9 +13,50 @@ const ToDoListItem = ({
     allToDos,
     setAllToDos
 
-  }) => {
+}) => {
+
+
     const [itemText, setItemText] = useState(text);
+    const [numRows, setNumRows] = useState(1);
     const [toDoChecked, setToDoChecked] = useState(checkbox);
+    const viewportWidth = window.innerWidth;
+
+    useEffect(() => {
+
+        let customNumRows;
+
+        switch (true) {
+            case viewportWidth <= 320:
+                customNumRows = 14;
+                break;
+            case viewportWidth > 320 && viewportWidth <= 375:
+                customNumRows = 19;
+                break;
+            case viewportWidth > 375 && viewportWidth <= 425:
+                customNumRows = 23;
+                break;
+            case viewportWidth > 425 && viewportWidth <= 768:
+                customNumRows = 47;
+                break;
+            case viewportWidth > 768 && viewportWidth <= 1024:
+                customNumRows = 66;
+                break;
+            case viewportWidth > 1024 && viewportWidth <= 1440:
+                customNumRows = 98;
+                break;
+            case viewportWidth > 1440 && viewportWidth <= 2560:
+                customNumRows = 194;
+                break;
+            default:
+                console.warn('No viewport width detected');
+                break;
+        }
+        const maxNumRows = Math.ceil(Math.max(1, itemText.length / customNumRows));
+
+        setNumRows(maxNumRows);
+    }, [itemText, viewportWidth])
+
+
 
     return (
         <div style={{width: '100%', borderRadius: '10px', display: 'flex', padding: '10px', boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.3)'}}>
@@ -33,13 +74,15 @@ const ToDoListItem = ({
                     </div>
                 </div>
                 <div style={{width: '75%'}}>
+
                     <label htmlFor={`text${itemKey}`} />
-                    <input
-                        style={{width: '100%', padding: '10px', border: '1px solid #fff'}}
+                    <textarea
+                        style={{ width: '100%', padding: '10px', border: '1px solid #fff', resize: 'none' }}
                         id={`text${itemKey}`}
                         type='text' value={itemText} placeholder='Enter to-do here'
+                        rows={numRows}
                         onChange={e => handleToDoContentChange(
-                    e.target.value, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
+                            e.target.value, itemKey, setItemText, setChangedToDoItem, setToDoChecked)}
                     />
                 </div>
 
