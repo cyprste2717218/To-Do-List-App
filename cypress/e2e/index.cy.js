ee6083284aefa15cf9bcee39a0e45be392d4453e
Stephen Ingham <smi505@york.ac.uk>
2024-08-07 22:54:35 +0100

    chore: added data-test prop for drawer element and finished off initial index tests

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,38 +1,27 @@
-describe('Navbar renders correctly', () => {
-  it('app loads', () => {
-    cy.visit("http://localhost:3000")
-  })
+describe('initial loading tests', () => {
 
-    it('Navbar exists', () => {
-     cy.getByData("to-do-app-header").should('exist')
-    })
-   
-
-
-  it('Title exists and contains correct text', () => {
-    cy.getByData("hero-heading").contains('To-Dos')
-  })
-
-  it('More options button renders', () => {
-    cy.getByData("more-options-elipsis-button").should('exist')
-  })
-
-
-
-
- 
-
-
+  it('Navbar exists', () => {
+    cy.getByData("to-do-app-header").should('exist')
+   })
   
-})
-
-
-describe('other', () => {
-  it('Add To-Do button renders', () => {
-    cy.getByData("add-to-do-button").should('exist')
+  it('Title exists and contains correct text', () => {
+   cy.getByData("hero-heading").contains('To-Dos')
+  })
+  
+  it('More options button renders', () => {
+   cy.getByData("more-options-elipsis-button").should('exist')
   })
 
   it('Options Drawer renders when pressed', () => {
-
+    cy.getByData("more-options-elipsis-button").click()
+    cy.getByData("off-canvas-drawer").should('exist')
   })
+  
+  it('Add To-Do button renders', () => {
+    cy.getByData("add-to-do-button").should('exist')
+  })
+  
+
 })
+
+
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+beforeEach(() => {
+	cy.visit("http://localhost:3000")
+  })
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
