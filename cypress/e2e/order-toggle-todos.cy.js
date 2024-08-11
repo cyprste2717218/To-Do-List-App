072954ab2812c3db62eb27b5fba9bbaf68ebfad9
Stephen Ingham <smi505@york.ac.uk>
2024-08-11 16:03:40 +0100

    chore: deleted placeholder e2e test in order toggle to dos spec and finished remaining toggle by completion e2e test

--- cypress/e2e/order-toggle-todos.cy.js
+++ cypress/e2e/order-toggle-todos.cy.js
@@ -1,13 +1,50 @@
-describe('User can add and order to-dos by completion status', () => {
+describe('User can perform to-do ordering operations', () => {	
 	
+	it('User can toggle to-dos to or not to display by completion status', () => {
+		
+		cy.addSampleToDosWithText()
+		cy.validateSampleAddedToDos()
 
-	
+		// checking to-dos and validating this
+		cy.getByData("to-do-list-item-checkbox")
+			.eq(0)
+			.click()
+		cy.getByData("to-do-list-item-checkbox")
+			.eq(2)
+			.click()
+
+        cy.validateIsChecked(1)
+        cy.validateIsChecked(3)
+
+		cy.validateSortingEnabled()
+
+		// check to-do item order is re-arranged to order checked to-dos before unchecked to-dos
+        cy.getByData("to-do-list-item-checkbox")
+            .eq(0)
+            .should('have.attr', 'aria-checked', 'true')
+
+        cy.getByData("to-do-list-item-checkbox")
+            .eq(1)
+            .should('have.attr', 'aria-checked', 'true')
+
+		// turn off sorting by completion  and check order is as it was before any to-dos checked
+		cy.setSortingDisabled()
+
+		cy.validateIsChecked(1)
+        cy.validateIsChecked(3)
+		cy.getByData("to-do-list-item-checkbox")
+			.eq(0)
+			.should('have.attr', 'aria-checked', 'true')
+
+		cy.getByData("to-do-list-item-checkbox")
+			.eq(2)
+			.should('have.attr', 'aria-checked', 'true')
+
+		
+	})
+
+
 })
 
 
 
-describe('User can toggle to-dos to or not to display by completion status', () => {
-	
-
-	
-})
